301f5b92a6591cd834bd2c4333347d61debfd5c3
Ruben DESERT <ruben.desert@eemi.com>
2016-11-09 14:31:13 +0100

    Add files via upload

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,5 +1,7 @@
-$('#popup').animate({bottom: '0px'}, 1000);
+if( window.isAdblock === undefined ){
+	$('#popup').animate({bottom: '0px'}, 1000);
 
-$('#pop-close').click(function(){
-	$('#popup').animate({bottom: '-350px'}, 1500)
-});
+	$('#pop-close').click(function(){
+		$('#popup').animate({bottom: '-350px'}, 1500)
+	});
+}
